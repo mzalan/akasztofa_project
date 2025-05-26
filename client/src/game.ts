d3d70d4ac5991bb7a0926837bcb86607cad8f912
mzalan <marquetant.zalan@students.jedlik.eu>
2025-05-26 10:34:28 +0200

    game felület wip

---
+++ client/src/game.ts
@@ -0,0 +1,16 @@
+// import IMessageProtocol from './IMessageProtocol';
+import './style.css';
+
+const alphabetDiv = document.querySelector('#alphabet-container') as HTMLDivElement;
+const alphabet = [
+    'A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H',
+    'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö',
+    'Ő', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű',
+    'V', 'W', 'X', 'Y', 'Z'
+  ];
+
+alphabet.forEach(letter => {
+    let button = document.createElement("button") as HTMLButtonElement;
+    button.innerText = letter;
+    alphabetDiv.append(button);
+})
